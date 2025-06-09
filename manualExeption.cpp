20ba8c9721a017025305577f9d28b817e561d79d
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-06-09 19:13:20 +0700

    last commit

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di prodi TI UMY" << endl;
+        throw("hallo"); // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dii eksekusi" << endl;     
+    }
+    catch(int a)
+   {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+   }
+   catch (...)
+   {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "default Pengecualian diexekusi" << endl;
+   }
+   return 0;
+}
