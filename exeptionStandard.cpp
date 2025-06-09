36e06143d84226ffaee1666f24318ecae8eb41c6
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-06-09 19:14:40 +0700

    last commit

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -14,5 +14,13 @@ int main()
         cout << data.at(4) << endl;
         //memanggil array
     }
-    
+    catch (exception &e)
+    {
+        //penangkap menggunakan obyek
+        cout << e.what() << endl;
+        /*akan di eksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris program yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 }
