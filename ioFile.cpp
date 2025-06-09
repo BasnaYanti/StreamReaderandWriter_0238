c834ff2c222f846c9220b9c092e003e607a03c16
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-06-09 17:27:05 +0700

    membuat obyek output file stream dalam mode menulis sekaligus membuka filenya

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,19 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string baris;
+
+    // membuat obyek output file stream dalam mode menulis. 
+    ofstream output;
+    // membuka file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+    
+
+    
+}
