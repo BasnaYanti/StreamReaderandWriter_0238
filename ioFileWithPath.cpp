53b8355b6db1f621e6a7563d38d464c872258a17
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-06-09 18:55:50 +0700

    membuat obyek file stream dalam mode menulis sekaligus membuka file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -8,5 +8,14 @@ int main()
     string baris;
     string namaFile;
 
-    
+    cout << "Enter FileName : ";
+    cin >> namaFile;
+
+    // membuat obyek output file stream dalam mode menulis. 
+    ofstream outfile;
+    // membuka file
+    outfile.open("namaFile + .txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
 }
