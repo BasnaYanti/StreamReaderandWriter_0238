7d0137e7d938136f0a52fee4734e4aa6633f3234
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-06-09 17:38:07 +0700

    membuat perulangan untuk setiap baris yang di ketik akan di simpan di file

--- ioFile.cpp
+++ ioFile.cpp
@@ -14,7 +14,25 @@ int main()
 
     cout << ">= Menulis file, \'q\' untuk keluar " << endl;
 
-    
+    // unlimited loop untuk menulis
+    while(true)
+    {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        gateline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")
+            break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.clode();
 
-    
+    // membuat obyek input fille
+    ifstream infile;
+    // membuka file
+    infile.open("contohfile.txt");
+
+    cout << endl;
 }
